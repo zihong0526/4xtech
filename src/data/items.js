8c9b64631ab15a6ffb2b35520b3a42ecf76fa72a
zihong0526 <149786427+zihong0526@users.noreply.github.com>
2025-04-08 17:07:12 +0800

    Updated hero section style

--- src/data/items.js
+++ src/data/items.js
@@ -5,7 +5,7 @@ const contactInfo = {
     address: '4x Technologies Pte. Ltd. Blk 3024 Ubi Road 3 #02-51 Kampong Ubi Industrial Estate Singapore 408652',
     phone1: '+6590231687',
     email: 'xinxiaodong@4xtechnologies.com.sg',
-    website: '4xtechnologies.netify.app',
+    website: '4xtechnologies.com.sg',
     whatsapp: '+6590231687',
 }
 
